b44bc88caa157ec52dab584bb1daf64a80c74299
Piotr Gródek <ipc0nfg@gmail.com>
2020-01-08 19:31:22 +0000

    Prepare to write initial implementation

--- vilga/include/vilga/vilga.h
+++ vilga/include/vilga/vilga.h
@@ -4,7 +4,47 @@
 
 namespace vilga {
 
-void foo();
+class header {
+
+};
+
+class datablob {
+
+};
+
+class data {
+  header   header;
+  datablob data;
+};
+
+struct logger_setup {
+  bool is_enabled;
+};
+
+/**
+ * Main logger class which provides interface for sending data
+ */
+class logger {
+public:
+
+  logger(logger_setup) {
+
+  }
+
+  template<class ...Args>
+  void log(const Args &... args) {
+    if (!setup_.is_enabled) return;
+    {
+      memory_buf_t buf;
+      fmt::format_to(buf, fmt, args...);
+      details::log_msg log_msg(loc, name_, lvl, string_view_t(buf.data(), buf.size()));
+      log_it_(log_msg, log_enabled, traceback_enabled);
+    }
+  }
+
+private:
+  logger_setup setup_;
+};
 
 }  // namespace vilga
 
---
+++ vilga/testbed.cpp
@@ -0,0 +1,8 @@
+// Simple app for testing during development.
+
+#include <vilga/vilga.h>
+
+int main(int argc, char** argv) {
+
+  return 0;
+}
